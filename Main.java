3b2781f5e59ffc9d62c8a03f288ff4abcafcd0c2
MjunMJ <84695386+MjunMJ@users.noreply.github.com>
2024-05-06 10:04:40 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static String sortWord(String string){
+        char[] chr=string.toCharArray();
+        Arrays.sort(chr);
+        return String.valueOf(chr);
+
+    }//end word sorter
+    public static void main(String[] args) {
+        String[] input = {"tar", "rat","art","meats","steam"};
+        Map<String, List<String>> sortWordPair = new HashMap<>();
+        List<String> words = new ArrayList<String>();
+        for (String l:input){
+            String key = sortWord(l);
+            sortWordPair.put(key, words);
+        }
+        System.out.println(sortWordPair.get("art"));
+        for (String l:input) {
+            String key = sortWord(l);
+            if (sortWordPair.containsKey(key)) {
+                sortWordPair.get(key).add(l);
+            }
+        }
+        System.out.println(sortWordPair);
+    }//end main
+}
